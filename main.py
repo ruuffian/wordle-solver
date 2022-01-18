036a10f45ed1fa330c6939336f4bec594fb99e1a
ruuffian <ruuffian@gmail.com>
2022-01-18 10:09:55 -0500

    hashmap optimization in get_unique and general naming conventions cleaned up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def load_words():
-    with open('words_alpha.txt') as word_file:
+    with open('words_alpha.txt.txt') as word_file:
         word_set = set(word_file.read().split())
     valid_words = [val for val in word_set if len(val) == 5]
     return valid_words
@@ -19,13 +19,15 @@ def contain_char(val, bl):
     return False
 
 
-# make n-time with hashmap
 def uniqueness_score(wordin):
     score = 5
+    letters = {}
     for i in range(0, 5):
-        for j in range(1, 5):
-            if i != j and i < j and wordin[i] == wordin[j]:
-                score -= 1
+        if wordin[i] in letters.keys():
+            letters[wordin[i]] += 1
+            score -= 1
+        else:
+            letters[wordin[i]] = 1
     return score
 
 
@@ -34,25 +36,26 @@ def get_unique(wordlist):
     for i in wordlist:
         values[i] = uniqueness_score(i)
     tmp = 0
-    unique = ""
+    maxstr = ""
     for i in values:
         if values[i] > tmp:
             tmp = values[i]
-            unique = i
-    return unique
+            maxstr = i
+    return maxstr
 
 
-def lst_refine(lst, wordin, result, bl, yllw, crrct):
+def lst_refine(wordin, lst, bl, yllw, crrct):
     for i in range(0, 5):
-        if result[i] == "b":
+        if wordle[i] == "b":
             bl.append(wordin[i])
-        elif result[i] == "y":
+        elif wordle[i] == "y":
             yllw.append(wordin[i])
         else:
             crrct[i] = wordin[i]
-    noblacklist = [val for val in lst if not contain_char(val, bl)]
-    allcorrectletters = [val for val in noblacklist if contain_char(val, yllw)]
-    possiblewords = [val for val in allcorrectletters if correct_check(val, crrct)]
+    possiblewords = [val for val in lst if
+                     not contain_char(val, bl) and contain_char(val, yllw) and correct_check(val, crrct)]
+    # allcorrectletters = [val for val in noblacklist if contain_char(val, yllw)]
+    # possiblewords = [val for val in allcorrectletters if correct_check(val, crrct)]
     return {
         "blacklist": bl,
         "yellow": yllw,
@@ -63,27 +66,26 @@ def lst_refine(lst, wordin, result, bl, yllw, crrct):
 
 if __name__ == '__main__':
     print("Welcome to ruuffian's wordle solver!")
-    print("To start, try audio.")
-    word = "audio"
+    print("What word did you start with? I suggest something with lots of vowels, like audio or adieu.")
+    usrin = input()
     print("What was the wordle output?")
     wordle = input()
     english_words = load_words()
     blacklist = []
     yellow = []
     correct = ["0", "0", "0", "0", "0"]
-    masterlst = lst_refine(english_words, word, wordle, blacklist, yellow, correct)
+    masterlst = lst_refine(usrin, english_words, blacklist, yellow, correct)
     count = 1
-    word = get_unique(masterlst["lst"])
-    print("Try " + word)
-    while len(masterlst["lst"]) != 1 and count <= 6:
+    unique = get_unique(masterlst["lst"])
+    print("Try " + unique)
+    while len(masterlst["lst"]) > 0 and count <= 6:
         print("What was the wordle output?")
         wordle = input()
         if "y" not in wordle and "b" not in wordle:
             print("Congratulations! We got the answer in " + str(count) + " tries!!")
             break
-        masterlst = lst_refine(masterlst["lst"], word, wordle, masterlst["blacklist"],
-                               masterlst["yellow"], masterlst["correct"])
+        masterlst = lst_refine(unique, masterlst["lst"], masterlst["blacklist"], masterlst["yellow"], masterlst["correct"])
         count += 1
-        word = get_unique(masterlst["lst"])
-        print("Try " + get_unique(masterlst["lst"]))
+        unique = get_unique(masterlst["lst"])
+        print("Try " + unique)
     print("Thank you for using ruuffian's wordle solver. I hope it worked out!")
